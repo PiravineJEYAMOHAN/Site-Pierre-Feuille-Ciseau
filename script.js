ff6e064eedfa2972b08096739316941cf482551c
Deus77777 <118898059+Deus77777@users.noreply.github.com>
2023-04-01 15:51:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const eloElement = document.getElementById("elo");
+let elo = 0;
+
 function play(userChoice) {
     let computerChoice = Math.random();
     if (computerChoice < 0.34) {
@@ -8,31 +11,44 @@ function play(userChoice) {
         computerChoice = "ciseaux";
     }
     let result = compare(userChoice, computerChoice);
+
+    if(result == "Gagné!")
+    {
+        elo += 15;
+    }else if(result == "Perdu!")
+    {
+        elo = Math.max(elo - 15, 0);
+    }
+
     document.getElementById("result").innerHTML = result;
+    eloElement.textContent = elo;
 }
+
+
+//Determine le résultat de l'échange
 function compare(choice1, choice2) {
     if (choice1 === choice2) {
         return "Égalité!";
     }
     if (choice1 === "pierre") {
         if (choice2 === "ciseaux") {
-            return "Vous avez gagné!";
+            return "Gagné!";
         } else {
-            return "L'ordinateur a gagné!";
+            return "Perdu!";
         }
     }
     if (choice1 === "feuille") {
         if (choice2 === "pierre") {
-            return "Vous avez gagné!";
+            return "Gagné!";
         } else {
-            return "L'ordinateur a gagné!";
+            return "Perdu!";
         }
     }
     if (choice1 === "ciseaux") {
         if (choice2 === "feuille") {
-            return "Vous avez gagné!";
+            return "Gagné!";
         } else {
-            return "L'ordinateur a gagné!";
+            return "Perdu!";
         }
     }
 }
