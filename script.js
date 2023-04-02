e78333630aa0d1d62bb6cf4b9e5293c7648a8be3
Deus77777 <118898059+Deus77777@users.noreply.github.com>
2023-04-02 13:12:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,46 @@
 const eloElement = document.getElementById("elo");
 let elo = 0;
 
+const pierreBtn = document.getElementById('rock');
+const feuilleBtn = document.getElementById('paper');
+const ciseauBtn = document.getElementById('scissors');
+
 function play(userChoice) {
+    
+    // Donne un choix au hasard à l'ordinateur
     let computerChoice = Math.random();
     if (computerChoice < 0.34) {
-        computerChoice = "pierre";
+        computerChoice = "Pierre";
     } else if (computerChoice <= 0.67) {
-        computerChoice = "feuille";
+        computerChoice = "Feuille";
     } else {
-        computerChoice = "ciseaux";
+        computerChoice = "Ciseaux";
     }
+
+    //Affiche l'échange de coup
+    var gameDiv = document.getElementById('game');
+    var pierreBtn = document.getElementById('rock-btn');
+    var feuilleBtn = document.getElementById('paper-btn');
+    var ciseauBtn = document.getElementById('scissors-btn');
+    pierreBtn.style.display = 'none';
+    feuilleBtn.style.display = 'none';
+    ciseauBtn.style.display = 'none'
+
+    var userChoiceImg = document.createElement('img');
+    userChoiceImg.src = userChoice + '.png';
+    userChoiceImg.width = 100;
+    userChoiceImg.height = 100;
+    userChoiceImg.classList.add('choice-img');
+    gameDiv.appendChild(userChoiceImg);
+    
+    var computerChoiceImg = document.createElement('img');
+    computerChoiceImg.src = computerChoice + '.png';
+    computerChoiceImg.width = 100;
+    computerChoiceImg.height = 100;
+    computerChoiceImg.classList.add('choice-img');
+    gameDiv.appendChild(computerChoiceImg);
+
+    //Appelle compare pour déterminer le gagnant et change le elo
     let result = compare(userChoice, computerChoice);
 
     if(result == "Gagné!")
@@ -22,32 +53,46 @@ function play(userChoice) {
         elo = Math.max(elo - 15, 0);
     }
 
+    //Affiche le gagnant et le nouveau elo
     document.getElementById("result").innerHTML = result;
     eloElement.textContent = elo;
+
+    // Attendre 4 secondes avant de réafficher les boutons
+    setTimeout(function() {
+        var choicesImgs = document.getElementsByClassName('choice-img');
+        while (choicesImgs.length > 0) {
+            choicesImgs[0].parentNode.removeChild(choicesImgs[0]);
+        }
+        pierreBtn.style.display = 'inline-block';
+        feuilleBtn.style.display = 'inline-block';
+        ciseauBtn.style.display = 'inline-block';
+        document.getElementById("result").innerHTML = '';
+    }, 3000);
 }
 
 
 //Determine le résultat de l'échange
+
 function compare(choice1, choice2) {
     if (choice1 === choice2) {
         return "Égalité!";
     }
-    if (choice1 === "pierre") {
-        if (choice2 === "ciseaux") {
+    if (choice1 === "Pierre") {
+        if (choice2 === "Ciseaux") {
             return "Gagné!";
         } else {
             return "Perdu!";
         }
     }
-    if (choice1 === "feuille") {
-        if (choice2 === "pierre") {
+    if (choice1 === "Feuille") {
+        if (choice2 === "Pierre") {
             return "Gagné!";
         } else {
             return "Perdu!";
         }
     }
-    if (choice1 === "ciseaux") {
-        if (choice2 === "feuille") {
+    if (choice1 === "Ciseaux") {
+        if (choice2 === "Feuille") {
             return "Gagné!";
         } else {
             return "Perdu!";
@@ -67,3 +112,4 @@ toggleThemeBtn.addEventListener('click', function() {
     toggleThemeBtn.textContent = 'Mode sombre temporaire';
   }
 });
+
